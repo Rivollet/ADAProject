4a1558f914ea25b8d08a67de5de53a3d1afd27fd
Loïc Pierre Antoine Veyssiere <loic.veyssiere@epfl.ch>
2017-01-25 13:41:06 +0100

    add dimension and maps

--- website/static/js/graphs.js
+++ website/static/js/graphs.js
@@ -1,41 +1,94 @@
-queue()
-    .defer(d3.json, "data/data.json")
+var q = queue()
+    .defer(d3.json, "data/data_mock.json")
     .defer(d3.json, "static/geojson/countries.json")
     .defer(d3.json, "static/geojson/us-states.json")
     .await(makeGraphs);
 
+/* Listeners */
+d3.select('#geo_selection').on("change", function() {
+  q.await(makeGraphs);
+});
+
+$(window).resize(function() {
+    clearTimeout(window.resizedFinished);
+    window.resizedFinished = setTimeout(function(){
+        console.log('Resized finished.');
+        q.await(makeGraphs);
+    }, 250);
+});
+
+/* Charts */
 function makeGraphs(error, data, geo_countries, geo_states) {
 
-  console.log(data);
-  console.log(geo_states);
+  var geo_type = getGeoType();
+  var geo_map = null;
+  var projection = null
 
-  var ndx = crossfilter(data['features']);
+  var width_map = getWidth();
+  var height_map = getWidth() / 1.8;
 
-  var stateDim = ndx.dimension(function(d) { return d["properties"]['STATE']; });
+
+  if (geo_type === "country") {
+    geo_map = geo_countries;
+    projection = d3.geo.mercator().scale((width_map) / (2 * Math.PI)).translate([width_map / 2, height_map / 2]);
+  }
+  else if (geo_type === "state") {
+    geo_map = geo_states;
+    projection = d3.geo.albersUsa().scale(width_map).translate([width_map / 2, height_map / 2]);
+  }
+
+  var ndx = crossfilter(data);
+
+  var stateDim = ndx.dimension(function(d) { return d['geo']; });
   console.log(stateDim);
-  var totalDonationsByState = stateDim.group();
+  var totalAmountByState = stateDim.group().reduceSum(function(d) {
+		return d["calories"];
+	});
+
+  var total = ndx.groupAll().reduceSum(function(d) {return d["calories"];});
+
+  var max_state = totalAmountByState.top(1)[0].value;
+  console.log(ndx.size())
 
 
   var usChart = dc.geoChoroplethChart("#us-chart");
+  var totalDonationsND = dc.numberDisplay("#total-donations-nd");
 
-  usChart.width(1000)
-    .height(330)
+  totalDonationsND
+    .formatNumber(d3.format("d"))
+    .valueAccessor(function(d){return d; })
+    .group(total)
+    .formatNumber(d3.format(".3s"));
+
+  usChart.width(width_map).height(height_map)
     .dimension(stateDim)
-    .group(totalDonationsByState)
+    .group(totalAmountByState)
     .colors(["#E2F2FF", "#C4E4FF", "#9ED2FF", "#81C5FF", "#6BBAFF", "#51AEFF", "#36A2FF", "#1E96FF", "#0089FF", "#0061B5"])
-    .colorDomain([0, 10])
-    .overlayGeoJson(geo_states["features"], "state", function (d) {
+    .colorDomain([0, max_state])
+    .overlayGeoJson(geo_map["features"], geo_type, function (d) {
         return d.properties.name;
     })
-    .projection(d3.geo.albersUsa()
-                .scale(600)
-                .translate([340, 150]))
+    .projection(projection)
     .title(function (p) {
         return "State: " + p["key"]
                 + "\n"
-                + "Total Donations: " + Math.round(p["value"]) + " $";
+                + "Total: " + Math.round(p["value"]) + " $";
     })
 
     dc.renderAll();
 
 };
+
+
+function updateData() {
+  console.log("test")
+}
+
+/* Private function */
+function getGeoType() {
+  return d3.select('#geo_selection input[type="radio"]:checked').property("value");
+}
+
+function getWidth() {
+  return $("#map_container").width();
+}
