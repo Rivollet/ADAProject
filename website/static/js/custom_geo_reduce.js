e55bc4885eb0a31e2f89b5838c84813c17b78781
Loïc Pierre Antoine Veyssiere <loic.veyssiere@epfl.ch>
2017-01-28 23:52:21 +0100

    some correction in version semi up to date

---
+++ website/static/js/custom_geo_reduce.js
@@ -0,0 +1,101 @@
+/* =============================================================================
+  GEO REDUCTION
+============================================================================= */
+
+function reduce_add_geo(metadata, global) {
+    return function(p, v) {
+
+        ++p[v.key].count;
+        p[v.key].sum_value += v.nbRecipes * v.value;
+        p[v.key].sum_weight += v.nbRecipes;
+        p[v.key].avg = p[v.key].sum_value / p[v.key].sum_weight;
+        p[v.key].value = p[v.key].avg / global[v.key];
+
+        update_final_value(p, v)
+        return p;
+    }
+}
+
+function reduce_remove_geo(metadata, global) {
+    return function(p, v) {
+        /*
+          init = reduce_init_geo(metadata, global);
+          --p[v.key].count;
+          if (p[v.key].count <= 0) {
+              console.log("test")
+              p = init();
+          } else {
+              p[v.key].sum_value -= v.nbRecipes * v.value;
+              p[v.key].sum_weight -= v.nbRecipes;
+              p[v.key].avg = p[v.key].sum_value / p[v.key].sum_weight;
+              p[v.key].value = p[v.key].avg / global[v.key];
+          }
+          update_final_value(p, v)
+          */
+        p[v.key].count = 0;
+        p[v.key].sum_value = 0;
+        p[v.key].sum_weight = 0;
+        p[v.key].avg = 0;
+        p[v.key].value = 0;
+        update_final_value(p, v)
+        return p;
+    }
+}
+
+function reduce_init_geo(metadata, global) {
+    return function() {
+        init = {};
+        // add field information
+        for (var key in metadata.nutriment) {
+            init[key] = base_structure();
+        }
+        // add final information
+        init['final'] = {
+            count: 0,
+            sum_value: 0,
+            sum_weight: 0,
+            avg: 0,
+            value: 0
+        }
+        return init;
+    }
+}
+
+function base_structure() {
+    return {
+        count: 0,
+        sum_value: 0,
+        sum_weight: 0,
+        avg: 0,
+        value: 0
+    }
+}
+
+/* =============================================================================
+  REDUCTION RULE
+============================================================================= */
+
+function update_final_value(p, v) {
+    var count_sum = 0;
+    var selected_key = null;
+    for (var key in p) {
+        if (p[key].count > 0) {
+            count_sum++;
+        } else {
+            // DO NOTHING
+        }
+
+        if (p[key].count > 0) {
+            selected_key = key;
+        }
+    }
+    if (count_sum == 1) {
+        p.final.value = p[selected_key].value;
+        p.final.key = selected_key;
+    } else {
+        //console.log(v)
+        //console.log(p)
+        p.final.value = v['nbRecipes'];
+        p.final.key = null;
+    }
+}
--- website/static/js/graphs.js
+++ website/static/js/graphs.js
@@ -39,7 +39,7 @@ function makeGraphs(error, init_data, geo_countries, geo_states, metadata) {
     if (geo_type === "country") {
         geo_map = geo_countries;
         data = init_data['per_country'];
-        projection = d3.geo.mercator().scale((width_map) / (2 * Math.PI)).translate([width_map / 2, height_map / 2]);
+        projection = d3.geo.mercator().scale((width_map) / (2 * Math.PI)).translate([width_map / 2, height_map / 1.6]);
         map_bijection = function(d) {
             return d.properties.wb_a3;
         };
@@ -56,8 +56,7 @@ function makeGraphs(error, init_data, geo_countries, geo_states, metadata) {
     Data dimension
     =========================================================================== */
     melt_data = melt(data, ['geo_identifier', 'geo_name', 'geo_country_in_base', 'nbRecipes', 'database_name'], 'key', 'value');
-    //console.log(melt_data);
-    //var ndx = crossfilter(data);
+
     var ndx = crossfilter(melt_data);
     var global = init_data.global['Global aggregation'];
 
@@ -67,50 +66,61 @@ function makeGraphs(error, init_data, geo_countries, geo_states, metadata) {
     var nutriment_dim = ndx.dimension(function(d) {
         return d['key'];
     });
-    //nutriment_dim.filterFunction(nutriment_filter_dim(metadata));
+
     var filter = 'avg_nutrition_calories_amount';
 
-    /*var key_group = nutriment_dim.group().reduceSum(function(d) {
-        return d['value'];
-    });*/
     var key_group = nutriment_dim.group().reduce(
         reduce_add_wavg(global),
         reduce_remove_wavg(global),
         reduce_init_wavg());
 
     var key_filtered_group = group_filtering(key_group, metadata);
-    //console.log(key_group.all());
+
+
     var total_by_geo = geo_dim.group().reduce(
-        reduceAddWeightedAvg(filter, 'nbRecipes'),
-        reduceRemoveWeightedAvg(filter, 'nbRecipes'),
-        reduceInitWeightedAvg()).order(function(d) {
-        //console.log(d)
-        return d.avg;
+        reduce_add_geo(metadata, global),
+        reduce_remove_geo(metadata, global),
+        reduce_init_geo(metadata, global));
+    total_by_geo.order(function(p) {
+        return p.final.value;
     });
 
-    //var key_filtered_group = group_filtering(key_group, metadata);
-    //console.log(key_filtered_group);
+    var key_filtered_group = group_filtering(key_group, metadata);
 
 
     var nb_recipes = ndx.groupAll().reduce(reduce_add_sum(), reduce_remove_sum(), reduce_init_sum());
-    var avg_readyInMinutes = ndx.groupAll().reduce(reduce_add_avg('avg_readyInMinutes'), reduce_remove_avg('avg_readyInMinutes'), reduce_init_sum());
+    var avg_readyInMinutes = geo_dim.groupAll().reduce(reduce_add_avg('avg_readyInMinutes'), reduce_remove_avg('avg_readyInMinutes'), reduce_init_sum());
 
-    var total_max = total_by_geo.top(1)[0].value.avg;
+    //var total_max = total_by_geo.top(1)[0].value.avg;
+    var total_max = 400;
+    console.log(total_by_geo.all())
+    console.log(geo_dim);
 
     /* ===========================================================================
     Charts
     =========================================================================== */
-    var mapChart = dc.geoChoroplethChart("#us-chart");
-    var totalRecipes = dc.numberDisplay("#total-recipes");
-    var readyIn = dc.numberDisplay("#ready_in");
-    var nutriment = dc.rowChart("#resource-type-row-chart");
+    var map_chart = dc.geoChoroplethChart("#map-chart");
+    var nb_recipes_chart = dc.numberDisplay("#total-recipes");
+    var ready_in_chart = dc.numberDisplay("#ready_in");
+    var nutriment_chart = dc.rowChart("#resource-type-row-chart");
+    var top_countries_chart = dc.dataTable("#top_country_table");
 
-    nutriment
+    console.log(map_chart)
+
+    //console.log(nutriment_chart);
+
+
+    nutriment_chart
         .width(width_map)
         .height(height_map)
         .elasticX(true)
         .dimension(nutriment_dim)
         .group(key_filtered_group)
+        .addFilterHandler(function(filters, filter) {
+            filters.length = 0; // empty the array
+            filters.push(filter);
+            return filters;
+        })
         .valueAccessor(function(d) {
             return d.value.pourcentage;
         })
@@ -121,30 +131,57 @@ function makeGraphs(error, init_data, geo_countries, geo_states, metadata) {
         })
         .ticks(4);
 
-    totalRecipes
+
+    nb_recipes_chart
         .formatNumber(d3.format(".3s"))
         .valueAccessor(function(d) {
             return d.sum_value;
         })
         .group(nb_recipes)
 
-    readyIn.formatNumber(d3.format(".3s"))
+
+    ready_in_chart.formatNumber(d3.format(".3s"))
         .valueAccessor(function(d) {
             return d.avg;
         })
         .group(avg_readyInMinutes)
 
-    mapChart.width(width_map).height(height_map)
+    top_countries_chart.dimension(total_by_geo)
+        .size(5)
+        .group(function(d) {
+            return ""
+        })
+        .columns([{
+                label: 'Country',
+                format: function(d) {
+                    return d.key;
+                }
+            },
+            {
+                label: 'Value',
+                format: function(d) {
+                    return d.value.final.value;
+                }
+            }
+        ])
+        .order(d3.descending);
+
+
+    map_chart.width(width_map).height(height_map)
         .dimension(geo_dim)
         .group(total_by_geo)
         .colors(['#DDDDDD', "#E2F2FF", "#C4E4FF", "#9ED2FF", "#81C5FF", "#6BBAFF", "#51AEFF", "#36A2FF", "#1E96FF", "#0089FF", "#0061B5"])
         //.colors(["#FFEDED", "#FFD5D5", "#FFBEBE", "#FFA6A6", "#FF8F8F", "#FF7777", "#FF6060", "#FF4848", "#FF3030", "#FF1919"])
-        .colorDomain([0, total_max])
         .valueAccessor(function(d) {
-            return d.value.avg;
+            console.log(d)
+            return d.value.final.value;
         })
-        .overlayGeoJson(geo_map["features"], geo_type, map_bijection)
-        .projection(projection);
+        .overlayGeoJson(geo_map.features, geo_type, map_bijection)
+        .projection(projection)
+        .title(function(d) {
+            return d.key;
+        });
+
 
     dc.renderAll();
 
